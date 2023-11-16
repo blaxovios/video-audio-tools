0a6d01093aa015914dcace18fe7bb49586821fe7
blaxovios <tsepe21@hotmail.com>
2023-11-16 12:26:31 +0200

    Removed cmd prints

--- remove_speech.py
+++ remove_speech.py
@@ -37,15 +37,13 @@ class AudioTools:
         mask_i = librosa.util.softmask(S_filter,
                                     margin_i * (S_full - S_filter),
                                     power=power)
-        print("mask_i.")
+
         mask_v = librosa.util.softmask(S_full - S_filter,
                                     margin_v * S_filter,
                                     power=power)
-        print("mask_v.")
+
         S_foreground = mask_v * S_full
-        print("S_foreground.")
         S_background = mask_i * S_full
-        print("S_background.")
         
         new_y = librosa.istft(S_background*phase)
         audio_no_speech_path = "static/exports/audio_only_without_speech.wav"
